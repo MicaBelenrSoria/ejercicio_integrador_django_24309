a667b654a276fcb87a65505775a6e94e9e0e9a20
MicaBelenrSoria <micabelenrs@gmail.com>
2024-05-26 18:41:16 -0300

    SE AGREGA A OPCION_2 DEL PUNTO 2

--- Punto 2/opcion_2.py
+++ Punto 2/opcion_2.py
@@ -9,6 +9,6 @@ def calcular_mcm(a, b):
 
 # Solicitar números al usuario
 numero1 = int(input("Ingrese el primer número por favor: "))
-numero2 = int(input("Ingrese el segundo número porfavor: "))
+numero2 = int(input("Ingrese el segundo número por favor: "))
 mcm = calcular_mcm(numero1, numero2)
 print(f"El MCM de {numero1} y {numero2} es {mcm}")
