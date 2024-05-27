46180e7ef510c33035aa0ec4d6f5ee2085fb3e05
MicaBelenrSoria <micabelenrs@gmail.com>
2024-05-27 00:57:50 -0300

    cambios punto 6

--- Punto 6/explicacion.py
+++ Punto 6/explicacion.py
@@ -1,10 +1,10 @@
 # En este sexto ejercicio hay que Crear una clase llamada Persona. Sus atributos son: nombre, edad y DNI. Construya los
 # siguientes métodos para la clase:
-#  Un constructor, donde los datos pueden estar vacíos.
-#  Los setters y getters para cada uno de los atributos. Hay que validar las entradas de
-# datos.
-#  mostrar(): Muestra los datos de la persona.
-#  Es_mayor_de_edad(): Devuelve un valor lógico indicando si es mayor de edad.
+# * Un constructor, donde los datos pueden estar vacíos.
+# * Los setters y getters para cada uno de los atributos. Hay que validar las entradas de
+#   datos.
+# * mostrar(): Muestra los datos de la persona.
+# * Es_mayor_de_edad(): Devuelve un valor lógico indicando si es mayor de edad.
 
 # Para implementar la clase Persona con los atributos nombre, edad y DNI, y los métodos solicitados en el enunciado lo analice de la siguiente forma:
 
